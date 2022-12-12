f8a6a8e928fd51f529578b0fc73af162b0d3980f
Aradhana <aradhanaabraham680@gmail.com>
2022-12-12 07:32:29 +0530

    web drivers file

--- src/test/java/seleniumcommands/BasicCommands.java
+++ src/test/java/seleniumcommands/BasicCommands.java
@@ -6,14 +6,30 @@ import org.openqa.selenium.edge.EdgeDriver;
 
 public class BasicCommands {
     public static void main(String[] args) {
+        WebDriver driver;
 
         System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"\\src\\test\\resources\\driverfiles\\chromedriver.exe");
 
 
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://selenium.obsqurazone.com/index.php");
+        driver  = new ChromeDriver();
         driver.manage().window().maximize();
+        driver.manage().deleteAllCookies();
+        driver.get("https://selenium.obsqurazone.com/index.php");
+        String sourcecode= driver.getPageSource();
+        System.out.println(sourcecode);
+        String CurrentUrl=driver.getCurrentUrl();
+        System.out.println(CurrentUrl);
+        String title= driver.getTitle();
+        System.out.println(title);
         driver.close();
+      //  driver=new EdgeDriver();
+       // System.setProperty("webdriver.edge.driver",System.getProperty("user.dir")+"\\src\\test\\resources\\driverfiles\\msedgedriver.exe");
+      //  driver.get("https://selenium.obsqurazone.com/index.php");
+      //  driver.manage().window().maximize();
+      //  driver.close();
+
+
+
 
 
 
