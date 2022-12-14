65036c2db2e827203c1e2fea18c193f3ec1af592
Aradhana <aradhanaabraham680@gmail.com>
2022-12-14 23:44:33 +0530

    uploading Basic commands2.java

--- src/test/java/seleniumcommands/BasicCommands2.java
+++ src/test/java/seleniumcommands/BasicCommands2.java
@@ -5,8 +5,9 @@ import org.openqa.selenium.edge.EdgeDriver;
 
 public class BasicCommands2 {
     public static void main(String[] args) {
+        WebDriver driver;
         System.setProperty("webdriver.edge.driver",System.getProperty("user.dir")+"\\src\\test\\resources\\driverfiles\\msedgedriver.exe");
-        WebDriver driver=new EdgeDriver();
+        driver =new EdgeDriver();
         driver.get("https://selenium.obsqurazone.com/index.php");
         driver.manage().window().maximize();
         driver.close();
