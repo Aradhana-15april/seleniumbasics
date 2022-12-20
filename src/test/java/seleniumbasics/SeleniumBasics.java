ac7e4c93742b7bf842532f462bbaaa09cb2dcca2
Aradhana <aradhanaabraham680@gmail.com>
2022-12-21 01:07:46 +0530

    uploading testcases

--- src/test/java/seleniumbasics/SeleniumBasics.java
+++ src/test/java/seleniumbasics/SeleniumBasics.java
@@ -35,16 +35,16 @@ public class SeleniumBasics {
 
     @AfterMethod
     public void tearDown() {
-        driver.close();
+       // driver.close();
     }
 
     @Test
     public void TC_001_verifyObsquraTitle() {
         driver.get("https://selenium.obsqurazone.com/index.php");
         String actualTitle = driver.getTitle();
-        String expectedTitle = "Obsqura Testing ";
+      String expectedTitle = "Obsqura Testing";
         Assert.assertEquals(actualTitle, expectedTitle, "Invalid Title found");
-    }
+   }
 
     @Test
     public void TC_002_verifySingleInputMessage() {
@@ -72,4 +72,54 @@ public class SeleniumBasics {
         String expectedTotal = "Total A + B : 3";
         Assert.assertEquals(actualtotal,expectedTotal,"Invalid total");
     }
-}
+
+    @Test
+    public void TC_004_verifyFormNoZipNoState(){
+        driver.get("https://selenium.obsqurazone.com/form-submit.php");
+        WebElement firstName = driver.findElement(By.xpath("//input[@class='form-control']"));
+        firstName.sendKeys("Aradhana");
+        WebElement lastName = driver.findElement(By.xpath("//input[@placeholder='Last name']"));
+        lastName.sendKeys("Abraham");
+        WebElement userName = driver.findElement(By.xpath("//input[@id='validationCustomUsername']"));
+        userName.sendKeys("aradhanaabraham680@gmail.com");
+        WebElement city = driver.findElement(By.xpath("//input[@placeholder='City']"));
+        city.sendKeys("Pathanamthitta");
+        WebElement zip = driver.findElement(By.xpath("//input[@id='validationCustom05']"));
+        //zip.sendKeys("123456");
+        WebElement state = driver.findElement(By.xpath("//input[@placeholder='State']"));
+        state.sendKeys("kerala");
+        WebElement checkBox = driver.findElement(By.xpath("//input[@class='form-check-input']"));
+        checkBox.click();
+        WebElement submit = driver.findElement(By.xpath("//button[@class='btn btn-primary']"));
+        submit.click();
+
+        WebElement zipValidation = driver.findElement(By.xpath("//input[@placeholder='Zip']"));
+        String actualZipValidation = zip.getText();
+        String expectedZipValidation = "Enter valid zip";
+
+        Assert.assertEquals(actualZipValidation,expectedZipValidation,"Invalid zip");
+
+    }
+    @Test
+    public void TC_005_verifySubmitform(){
+        driver.get("https://selenium.obsqurazone.com/form-submit.php");
+        WebElement firstName = driver.findElement(By.xpath("//input[@class='form-control']"));
+        firstName.sendKeys("Aradhana");
+        WebElement lastName = driver.findElement(By.xpath("//input[@placeholder='Last name']"));
+        lastName.sendKeys("Abraham");
+        WebElement userName = driver.findElement(By.xpath("//input[@id='validationCustomUsername']"));
+        userName.sendKeys("aradhanaabraham680@gmail.com");
+        WebElement city = driver.findElement(By.xpath("//input[@placeholder='City']"));
+        city.sendKeys("Pathanamthitta");
+        WebElement zip = driver.findElement(By.xpath("//input[@id='validationCustom05']"));
+        zip.sendKeys("123456");
+        WebElement state = driver.findElement(By.xpath("//input[@placeholder='State']"));
+        state.sendKeys("kerala");
+        WebElement checkBox = driver.findElement(By.xpath("//input[@class='form-check-input']"));
+        checkBox.click();
+        WebElement submit = driver.findElement(By.xpath("//button[@class='btn btn-primary']"));
+        submit.click();
+    }
+
+    }
+
