4e83e8bf08aeded1bc91646632a1a8a052aa07f3
Aradhana <aradhanaabraham680@gmail.com>
2022-12-22 06:05:54 +0530

    uploading file of testes

--- src/test/java/seleniumbasics/SeleniumBasics.java
+++ src/test/java/seleniumbasics/SeleniumBasics.java
@@ -35,7 +35,7 @@ public class SeleniumBasics {
 
     @AfterMethod
     public void tearDown() {
-       // driver.close();
+      // driver.close();
     }
 
     @Test
@@ -84,20 +84,20 @@ public class SeleniumBasics {
         userName.sendKeys("aradhanaabraham680@gmail.com");
         WebElement city = driver.findElement(By.xpath("//input[@placeholder='City']"));
         city.sendKeys("Pathanamthitta");
-        WebElement zip = driver.findElement(By.xpath("//input[@id='validationCustom05']"));
-        //zip.sendKeys("123456");
-        WebElement state = driver.findElement(By.xpath("//input[@placeholder='State']"));
-        state.sendKeys("kerala");
+        //WebElement state = driver.findElement(By.xpath("//input[@placeholder='State']"));
+       // state.sendKeys("kerala");
+
+        String expectedZipFieldErrorMessage = "Please provide a valid zip.";
         WebElement checkBox = driver.findElement(By.xpath("//input[@class='form-check-input']"));
         checkBox.click();
         WebElement submit = driver.findElement(By.xpath("//button[@class='btn btn-primary']"));
         submit.click();
-
-        WebElement zipValidation = driver.findElement(By.xpath("//input[@placeholder='Zip']"));
-        String actualZipValidation = zip.getText();
-        String expectedZipValidation = "Enter valid zip";
-
-        Assert.assertEquals(actualZipValidation,expectedZipValidation,"Invalid zip");
+        WebElement zipFieldValidation = driver.findElement(By.xpath("//input[@id='validationCustom05']//following-sibling::div[1]"));
+        String actualzipFieldValidation = zipFieldValidation.getText();
+        WebElement stateFieldValidation = driver.findElement(By.xpath("//input[@id='validationCustom04']//following-sibling::div[1]"));
+        String expectedStateFieldErrorMessage = "Please provide a valid state.";
+        Assert.assertEquals(stateFieldValidation.getText(), expectedStateFieldErrorMessage, "Invalid Message found");
+        Assert.assertEquals(actualzipFieldValidation,expectedZipFieldErrorMessage,"Invalid message found");
 
     }
     @Test
@@ -119,7 +119,78 @@ public class SeleniumBasics {
         checkBox.click();
         WebElement submit = driver.findElement(By.xpath("//button[@class='btn btn-primary']"));
         submit.click();
+        WebElement successValidationMessage = driver.findElement(By.xpath("//button[@class='btn btn-primary']/following-sibling::div"));
+        String actualSuccessMessage = successValidationMessage.getText();
+        System.out.println(actualSuccessMessage);
+        String expectedSuccessMessage = "Form has been submitted successfully!";
+
+       // WebElement submit = driver.findElement(By.xpath("//button[@class='btn btn-primary']"));
+        //submit.click();
+        Assert.assertEquals(actualSuccessMessage,expectedSuccessMessage,"Invalid message found");
+
+    }
+    @Test
+    public void TC_006_verifyValidationFieldEmpty(){
+        driver.get("https://selenium.obsqurazone.com/form-submit.php");
+        WebElement button = driver.findElement(By.xpath("//button[@class='btn btn-primary']"));
+        button.click();
+        WebElement firstnameFieldValidation = driver.findElement(By.xpath("//div[@class='invalid-feedback']"));
+        String expectedFirstNameErrorMessage = "Please enter First name.";
+        WebElement lastnameFieldValidation = driver.findElement(By.xpath("/html/body/section/div/div/div[2]/div/div/div[2]/form/div[1]/div[2]/div[1]"));
+        String expectedLastNameErrorMessage = "Please enter Last name.";
+        WebElement userNameValidation = driver.findElement(By.xpath("/html/body/section/div/div/div[2]/div/div/div[2]/form/div[1]/div[3]/div/div[2]"));
+        String expectedUserNameErrorMessage = "Please choose a username.";
+        WebElement cityValidation = driver.findElement(By.xpath("/html/body/section/div/div/div[2]/div/div/div[2]/form/div[2]/div[1]/div[1]"));
+        String expectedCityErrorMessage = "Please provide a valid city.";
+        WebElement stateFieldValidation = driver.findElement(By.xpath("/html/body/section/div/div/div[2]/div/div/div[2]/form/div[2]/div[2]/div[1]"));
+        String expectedStateFieldErrorMessage = "Please provide a valid state.";
+        WebElement zipFieldValidation = driver.findElement(By.xpath("/html/body/section/div/div/div[2]/div/div/div[2]/form/div[2]/div[3]/div[1]"));
+        String expectedZipFieldErrorMessage = "Please provide a valid zip.";
+        WebElement termsConditionFieldValidation = driver.findElement(By.xpath("//input[@id='invalidCheck']//following-sibling::div[1]"));
+        String expectedTermsConditionErrorMessage = "You must agree before submitting.";
+        button.click();
+        Assert.assertEquals(firstnameFieldValidation.getText(), expectedFirstNameErrorMessage, "Invalid Message found");
+        Assert.assertEquals(lastnameFieldValidation.getText(), expectedLastNameErrorMessage, "Invalid Message found");
+        Assert.assertEquals(userNameValidation.getText(), expectedUserNameErrorMessage, "Invalid Message found");
+        Assert.assertEquals(cityValidation.getText(), expectedCityErrorMessage, "Invalid Message found");
+        Assert.assertEquals(stateFieldValidation.getText(), expectedStateFieldErrorMessage, "Invalid Message found");
+        Assert.assertEquals(zipFieldValidation.getText(), expectedZipFieldErrorMessage, "Invalid Message found");
+        Assert.assertEquals(termsConditionFieldValidation.getText(), expectedTermsConditionErrorMessage, "Invalid Message found");
+
     }
 
+    @Test
+    public void TC_007_verifyNewsletterSubscription(){
+        driver.get("https://demowebshop.tricentis.com/");
+        WebElement newsletter = driver.findElement(By.cssSelector("#newsletter-email"));
+        newsletter.sendKeys("aradhanaabraham680@gmail.com");
+        WebElement subscribe = driver.findElement(By.cssSelector("#newsletter-subscribe-button"));
+        subscribe.click();
+
+    }
+    @Test
+    public void TC_008_verifyInstantdemoRequestform(){
+        driver.get("https://phptravels.com/demo/");
+        WebElement firstName = driver.findElement(By.cssSelector("input.first_name"));
+        firstName.sendKeys("Aradhana");
+        WebElement lastName = driver.findElement(By.cssSelector("input.last_name"));
+        lastName.sendKeys("Abraham");
+        WebElement businessName = driver.findElement(By.cssSelector("input.business_name"));
+        businessName.sendKeys("Aradhana Abraham");
+        WebElement email = driver.findElement(By.cssSelector("input.email"));
+        email.sendKeys("aradhanaabraham680@gmail.com");
+        WebElement value1 = driver.findElement(By.cssSelector("span#numb1"));
+        String valueA = value1.getText();
+        int number1 = Integer.parseInt(valueA);
+        WebElement value2 = driver.findElement(By.cssSelector("span#numb2"));
+        String valueB = value2.getText();
+        int number2 = Integer.parseInt(valueB);
+        WebElement resultField = driver.findElement(By.cssSelector("input#number"));
+        int result = number1+number2;
+        String Result1 = String.valueOf(result);
+        resultField.sendKeys(Result1);
+        WebElement submit = driver.findElement(By.cssSelector("button#demo"));
+        submit.click();
+    }
     }
 
